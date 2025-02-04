dd7327848320ff003136d01591f4f7b9b3899730
shaunLaren <codelaren@gmail.com>
2025-02-04 05:13:16 +0100

    Limit framerate

--- main.py
+++ main.py
@@ -6,13 +6,23 @@ def main():
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH} \nScreen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
-    while True:
-        screen.fill((0, 0, 0))
-        pygame.display.flip()
+    
+    # Main game loop
+    running = True
+    while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                return
+                running = False
+        
+        # Black background
+        screen.fill((0, 0, 0))
+        pygame.display.flip()
+        
+        # Tickrate
+        dt = clock.tick(60) / 1000
 
     
 if __name__ == "__main__":
